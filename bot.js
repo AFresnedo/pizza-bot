6607fcb6637aa2d67f436de15008e014ece258ec
AFresnedo <AFresnedo@users.noreply.github.com>
2018-11-13 11:16:05 -0800

    normalize indentation to 2 spaces

--- bot.js
+++ bot.js
@@ -11,43 +11,43 @@ const NEW_USER = 'newUserProperty';
 const GREETED_USER = 'greetUserProperty';
 
 class PizzaBot {
-  /**
-   *
-   * @param {ConversationState} conversation state containing convo history
-   * @param {UserState} state containing user-specific information
-   */
-  constructor(conversationState, userState) {
-    // Create a boolean to indicate if the user is brand new to the bot
-    this.newUserProperty = userState.createProperty(NEW_USER);
-    // Add given user state to this PizzaBot instance
-    this.userState = userState;
-    // Create a boolean to track if a user has been greeted in convo yet
-    this.greetProperty = conversationState.createProperty(GREETED_USER);
-    // Add given conversation state to this PizzaBot instance
-    this.conversationState = conversationState;
-  }
-  /**
-   *
-   * @param {TurnContext} on turn context object.
-   */
-  async onTurn(turnContext) {
-    // Perform message handling logic, if that type of event is detected
-    if (turnContext.activity.type === ActivityTypes.Message) {
-      await turnContext.sendActivity(`You said "${turnContext.activity.text}"`);
-    // Perform convo update logic, if that type of event is detected
-    } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
-      // For every member in conversation, welcome them if new
-      for (let e of turnContext.activity.membersAdded) {
-        // TODO Prompt with welcome message, if user is a first time user
-
-      }
-      await turnContext.sendActivity('Conversation Updated');
-    } else {
-      await turnContext.sendActivity(`[${turnContext.activity.type} event detected]`);
+    /**
+     *
+     * @param {ConversationState} conversation state containing convo history
+     * @param {UserState} state containing user-specific information
+     */
+    constructor(conversationState, userState) {
+        // Create a boolean to indicate if the user is brand new to the bot
+        this.newUserProperty = userState.createProperty(NEW_USER);
+        // Add given user state to this PizzaBot instance
+        this.userState = userState;
+        // Create a boolean to track if a user has been greeted in convo yet
+        this.greetProperty = conversationState.createProperty(GREETED_USER);
+        // Add given conversation state to this PizzaBot instance
+        this.conversationState = conversationState;
     }
-    // Save state changes
-    await this.conversationState.saveChanges(turnContext);
-  }
+    /**
+     *
+     * @param {TurnContext} on turn context object.
+     */
+    async onTurn(turnContext) {
+        // Perform message handling logic, if that type of event is detected
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            await turnContext.sendActivity(`You said "${turnContext.activity.text}"`);
+            // Perform convo update logic, if that type of event is detected
+        } else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            // For every member in conversation, welcome them if new
+            for (let e of turnContext.activity.membersAdded) {
+                // TODO Prompt with welcome message, if user is a first time user
+
+            }
+            await turnContext.sendActivity('Conversation Updated');
+        } else {
+            await turnContext.sendActivity(`[${turnContext.activity.type} event detected]`);
+        }
+        // Save state changes
+        await this.conversationState.saveChanges(turnContext);
+    }
 }
 
 module.exports.PizzaBot = PizzaBot;
